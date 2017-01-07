30b24cf6a8a9f1a03f46aa491112b8a56053eb48
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-07 17:58:24 -0500

    share data between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MoviesService } from './movies.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '¡Funciona la App!';
+
+  constructor(private service: MoviesService) { }
+
+  agregarPelicula() {
+    this.service.movies.push({
+      title: 'Frozen',
+      id: 'sdasda12'
+    });
+  }
 }
