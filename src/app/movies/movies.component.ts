61e1191ac59cc223fd25e07f4e29fc6af407930c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-07 17:31:45 -0500

    show data with ngFor

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from './movie';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
+  movies: Movie[];
 
   constructor() { }
 
   ngOnInit() {
+    this.movies = [
+      {title: 'Toy Story', id: '123'},
+      {title: 'Toy Story 2', id: '124'},
+      {title: 'Toy Story 3', id: '125'}
+    ];
   }
 
 }
